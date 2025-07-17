90d0ee2b81baac1c575d394fc60bc53bfc078f9f
Gabriel Cruz <gabriel.crzprz45@gmail.com>
2025-07-17 16:14:19 -0400

    updated the models to SQLAlchemy 2.0;

--- task_tracker/models.py
+++ task_tracker/models.py
@@ -1,32 +1,46 @@
 from . import db
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
+from typing import List, Optional
+from sqlalchemy import ForeignKey, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy import func
 
 # database model
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.now)
+class Todo(db.Model): # change 'Todo' to 'Task'
+    __tablename__ = 'todos'
 
-    # repr == representation; this is for debugging purposes, helps quickly identify the object when debugging
-    def __repr__(self): # returns a string everytime we create a new element
-        return '<Task %r>' % self.id
+    id: Mapped[int] = mapped_column(primary_key=True)
+    task: Mapped[str] = mapped_column(String(200))
+    date_created: Mapped[datetime.datetime] = mapped_column(
+    server_default=func.now()
+    )
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+
+    user: Mapped["User"] = relationship(back_populates="todos")
+
+    def __repr__(self) -> str:
+        return f"Todo(id={self.id!r}, task={self.task!r}, datetime={self.date_created!r})"
     
 
-class Users(db.Model):
-    __bind_key__ = 'users'
-    id =db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+
+class User(db.Model):
+    __tablename__ = 'users'
+    # __bind_key__ = 'users' # check for change on __init__.py
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(20), unique=True)
+    password_hash: Mapped[str] = mapped_column(String(128))
+
+    todos: Mapped[List["Todo"]] = relationship(back_populates="user")
+
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
-
-# ---------------------------------------------------------------IMPORTANT!----------------------------------------------------------------
-# It doesn't build a table or access a database. 
-# It simply registers the structure of your Todo table with that empty db placeholder. 
-# The db object now holds onto this "blueprint," waiting for further instructions.
-# -----------------------------------------------------------------------------------------------------------------------------------------
+    
+    def __repr__(self) -> str:
+        return f"User(id={self.id!r}, username={self.username!r})"
